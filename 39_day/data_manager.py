4407acbadd8186e31fd37918d32a7f8deab01801
Fuffes <Fuffes05@gmail.com>
2025-04-24 17:54:55 +0300

    39_day.

--- 39_day/data_manager.py
+++ 39_day/data_manager.py
@@ -11,6 +11,7 @@ class DataManager:
     def __init__(self):
         self._token = os.getenv("SHEETY_TOKEN")
         self.header = {"Authorization": f"Basic {self._token}"}
+        self.data = {}
 
 
     def get_data_from_table(self):
@@ -20,8 +21,8 @@ class DataManager:
                 list: A list of dictionaries containing table data (all records from the 'prices' sheet).
         """
         resp = requests.get(url=SHEETY_BASE_URL, headers=self.header).json()
-        result = resp["prices"]
-        return result
+        self.data = resp["prices"]
+        return self.data
 
 
     def add_data_to_table(self,  row_id: int, new_data: dict):
@@ -34,7 +35,6 @@ class DataManager:
 
         Returns:
             dict: API response after updating the data.
-
         """
         payload = {
             "price": new_data
--- 39_day/flight_search.py
+++ 39_day/flight_search.py
@@ -1,82 +1,65 @@
+import os
+
+import requests
+
+IATA_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+FLIGHT_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
 
 
+class FlightSearch:
+
+    def __init__(self):
+        self._access_token = self._get_access_token()
+        self._auth_header = {"Authorization" : f"Bearer {self._access_token}"}
 
 
+    def get_code(self, city_name, ):
+        query = {
+            "keyword": city_name,
+            "max": "2",
+            "include": "AIRPORTS",
+        }
+        response = requests.get(url=IATA_ENDPOINT, headers=self._auth_header, params=query)
+        try:
+            code = response.json()["data"][0]['iataCode']
+        except IndexError:
+            print(f"IndexError: No airport code found for {city_name}.")
+            return "N/A"
+        except KeyError:
+            print(f"KeyError: No airport code found for {city_name}.")
+            return "Not Found"
+        return code
 
 
+    def _get_access_token(self):
+        """
+            Generates the authentication token used for accessing the Amadeus API and returns it.
+            Returns:
+                str: The new access token obtained from the API response.
+        """
+        auth_url = "https://test.api.amadeus.com/v1/security/oauth2/token"
+        api_key = os.getenv("FLIGHT_SEARCH_API_KEY")
+        api_secret = os.getenv("FLIGHT_SEARCH_API_SECRET")
+        header = {"Content-Type":"application/x-www-form-urlencoded"}
+        body = f"grant_type=client_credentials&client_id={api_key}&client_secret={api_secret}"
+
+        resp = requests.post(url=auth_url, headers=header, data=body ).json()
+        return resp["access_token"]
 
 
+    def get_flight_data(self, destination, origin, from_time, to_time):
+        """
 
-
-# import os
-#
-# import requests
-# from dotenv import load_dotenv
-#
-# load_dotenv()
-# IATA_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
-# FLIGHT_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
-#
-# class FlightSearch:
-#     #This class is responsible for talking to the Flight Search API.
-#     def __init__(self):
-#         self._access_token = self._get_access_token()
-#         self._auth_header = {"Authorization" : f"Bearer {self._access_token}"}
-#
-#     def get_flight_data(self, destination, origin, from_time, to_time):
-#         """
-#
-#         """
-#         params = {
-#             "originLocationCode": origin,
-#             "destinationLocationCode": destination,
-#             "departureDate": from_time.strftime("%Y-%m-%d"),
-#             "returnDate": to_time.strftime("%Y-%m-%d"),
-#             "adults": 1,
-#             "nonStop": "true",
-#             "currencyCode": "USD",
-#             "max": "10",
-#         }
-#         resp = requests.get(url=FLIGHT_ENDPOINT, params=params, headers=self._auth_header)
-#         return resp.json()
-#
-#     def get_code(self, city_name):
-#         query = {
-#             "keyword": city_name,
-#             "max": "2",
-#             "include": "AIRPORTS",
-#         }
-#         response = requests.get(
-#             url=IATA_ENDPOINT,
-#             headers=self._auth_header,
-#             params=query
-#         )
-#         try:
-#             code = response.json()["data"][0]['iataCode']
-#         except IndexError:
-#             print(f"IndexError: No airport code found for {city_name}.")
-#             return "N/A"
-#         except KeyError:
-#             print(f"KeyError: No airport code found for {city_name}.")
-#             return "Not Found"
-#         return code
-#
-#
-#     def _get_access_token(self):
-#         """
-#             Generates the authentication token used for accessing the Amadeus API and returns it.
-#             This function makes a POST request to the Amadeus token endpoint with the required
-#             credentials (API key and API secret) to obtain a new client credentials token.
-#             Upon receiving a response, the function updates the FlightSearch instance's token.
-#             Returns:
-#                 str: The new access token obtained from the API response.
-#         """
-#         auth_url = "https://test.api.amadeus.com/v1/security/oauth2/token"
-#         api_key = os.getenv("FLIGHT_SEARCH_API_KEY")
-#         api_secret = os.getenv("FLIGHT_SEARCH_API_SECRET")
-#         header = {"Content-Type":"application/x-www-form-urlencoded"}
-#         body = f"grant_type=client_credentials&client_id={api_key}&client_secret={api_secret}"
-#
-#         resp = requests.post(url=auth_url, headers=header, data=body ).json()
-#         return resp["access_token"]
-#
+        """
+        params = {
+            "originLocationCode": origin,
+            "destinationLocationCode": destination,
+            "departureDate": from_time.strftime("%Y-%m-%d"),
+            "returnDate": to_time.strftime("%Y-%m-%d"),
+            "adults": 1,
+            "nonStop": "true",
+            "currencyCode": "USD",
+            "max": "10",
+        }
+        resp = requests.get(url=FLIGHT_ENDPOINT, params=params, headers=self._auth_header).json()
+        return resp
